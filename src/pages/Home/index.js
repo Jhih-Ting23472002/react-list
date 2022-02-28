2fedef97a79af8dc421e014922093ec8afcfc445
Jhih Ting <tina23472002@gmail.com>
2022-02-28 12:41:46 +0800

    練習

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,17 +1,24 @@
-import { useState } from "react";
+import { useState, useEffect } from 'react'
 
-import Edit from "./components/Edit";
-import List from "./components/List";
-import "./index.css";
+import Edit from './components/Edit'
+import List from './components/List'
+import './index.css'
 
 const Home = () => {
-  const [data, setData] = useState([]);
+  const [data, setData] = useState([])
+  useEffect(() => {
+    //綁定事件
+    return()=>{
+//取消綁定
+    }
+  }, [data])
+
   return (
     <div className="app">
-      <Edit add={setData}/>
-      <List listData={data} deleteData={setData}/>
+      <Edit add={setData} />
+      <List listData={data} deleteData={setData} />
     </div>
-  );
-};
+  )
+}
 
-export default Home;
+export default Home
